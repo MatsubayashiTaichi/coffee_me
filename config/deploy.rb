4168ec7145ddb24ec116f4a7488b2a4af7dbc198
MatsubayashiTaichi <dasnaiiwa.2547@gmail.com>
2019-06-18 01:14:32 +0900

    second commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "coffee_me"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:MatsubayashiTaichi/coffee_me.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
